f90e723f4f3d9a29613e48891f16d022138c2c5f
Gorodskoi <zuravlevrodion143@gmail.com>
2024-10-04 09:57:07 +0300

    Update help.php

--- editing/help.php
+++ editing/help.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AngusMarkets - Корзина</title>
+    <title>AngusMarkets - cart</title>
     <style>
         body {
             font-family: Arial, sans-serif;
